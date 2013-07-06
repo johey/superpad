cc5789d2641625be0c3eda8f72565ec44da1dca2
Johan Svensson <btcbank.js@crypt.se>
2013-07-06 16:47:04 +0200

    Added GPLv3 preamble to all source files

--- cd32.h
+++ cd32.h
@@ -1,3 +1,21 @@
+/***********************************************************************\
+* Superpad - a SNES/SFC joypad conversion to the Amiga platform         *
+* Copyright (C) 2013  Johan Svensson                                    *
+*                                                                       *
+* This program is free software: you can redistribute it and/or modify  *
+* it under the terms of the GNU General Public License as published by  *
+* the  Free  Software  Foundation, either version 3 of the License, or  *
+* any later version.                                                    *
+*                                                                       *
+* This program is distributed  in the hope that it will be useful, but  *
+* WITHOUT   ANY  WARRANTY;   without  even  the  implied  warranty  of  *
+* MERCHANTABILITY  or  FITNESS  FOR  A  PARTICULAR  PURPOSE.  See  the  *
+* GNU General Public License for more details.                          *
+*                                                                       *
+* You should  have received a copy  of the  GNU General Public License  *
+* along with this program. If not, see <http://www.gnu.org/licenses/>.  *
+\***********************************************************************/
+
 #ifndef cd32_h
 #define cd32_h
 
@@ -7,7 +25,10 @@ void runCd32();
 
 #define datashift(button) ((PINB & (1 << button)) << (7 - button))
 
-#define shiftData(button) waitLowClock(); if (signalSNES(button)) setHigh(AMIGA_DATA); else setLow(AMIGA_DATA); waitHighClock() 
+#define shiftData(button) waitLowClock(); if (signalSNES(button))\
+  setHigh(AMIGA_DATA); else setLow(AMIGA_DATA); waitHighClock() 
 
 #endif
+
+
 
--- mouse.c
+++ mouse.c
@@ -1,3 +1,21 @@
+/***********************************************************************\
+* Superpad - a SNES/SFC joypad conversion to the Amiga platform         *
+* Copyright (C) 2013  Johan Svensson                                    *
+*                                                                       *
+* This program is free software: you can redistribute it and/or modify  *
+* it under the terms of the GNU General Public License as published by  *
+* the  Free  Software  Foundation, either version 3 of the License, or  *
+* any later version.                                                    *
+*                                                                       *
+* This program is distributed  in the hope that it will be useful, but  *
+* WITHOUT   ANY  WARRANTY;   without  even  the  implied  warranty  of  *
+* MERCHANTABILITY  or  FITNESS  FOR  A  PARTICULAR  PURPOSE.  See  the  *
+* GNU General Public License for more details.                          *
+*                                                                       *
+* You should  have received a copy  of the  GNU General Public License  *
+* along with this program. If not, see <http://www.gnu.org/licenses/>.  *
+\***********************************************************************/
+
 #include "mouse.h"
 
 #define STATE  0b00110011 // Amiga mouse pulse mask
