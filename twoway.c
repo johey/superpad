7dac4a59c18403fbafc6c5d3a97b31e6aad9ee60
johey <github.js@crypt.se>
2013-07-07 11:04:24 +0200

    Added two-way mode where only directions right and left are active, plus Y=fire and B=up (for games where you jump with up, like Bubble Bobble)

---
+++ twoway.c
@@ -0,0 +1,33 @@
+/***********************************************************************\
+* Superpad - a SNES/SFC joypad conversion to the Amiga platform         *
+* Copyright (C) 2013  Johan Svensson                                    *
+*                                                                       *
+* This program is free software: you can redistribute it and/or modify  *
+* it under the terms of the GNU General Public License as published by  *
+* the  Free  Software  Foundation, either version 3 of the License, or  *
+* any later version.                                                    *
+*                                                                       *
+* This program is distributed  in the hope that it will be useful, but  *
+* WITHOUT   ANY  WARRANTY;   without  even  the  implied  warranty  of  *
+* MERCHANTABILITY  or  FITNESS  FOR  A  PARTICULAR  PURPOSE.  See  the  *
+* GNU General Public License for more details.                          *
+*                                                                       *
+* You should  have received a copy  of the  GNU General Public License  *
+* along with this program. If not, see <http://www.gnu.org/licenses/>.  *
+\***********************************************************************/
+
+#include "twoway.h"
+
+void runTwoway() {
+  setOutput(AMIGA_FIRE_CLK);
+  setOutput(AMIGA_SEL);
+  while (1) {
+    direction(SNESPAD_DIR_RIGHT, AMIGA_DIR_RIGHT);
+    direction(SNESPAD_DIR_LEFT, AMIGA_DIR_LEFT);
+    button(SNESPAD_B, AMIGA_DIR_UP);
+    button(SNESPAD_Y, AMIGA_FIRE_CLK);
+
+    selectMode();
+  }
+}
+
